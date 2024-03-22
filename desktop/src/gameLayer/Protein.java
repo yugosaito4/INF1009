f45b4d015213db590d61d34f399ebe705090b788
XINJINGG <xinjing-02@outlook.com>
2024-03-22 16:32:29 +0800

    Make sub classes

---
+++ desktop/src/gameLayer/Protein.java
@@ -0,0 +1,16 @@
+package gameLayer;
+
+import com.mygdx.game.Entity;
+import com.mygdx.game.Player;
+import com.mygdx.game.iCollider;
+
+public class Protein extends HealthyFood{
+	
+//private boolean eaten = false;
+	
+	public Protein(String imagePath, float x, float y, float s) {
+		super(imagePath, x, y, s);
+	}
+
+
+}
---
+++ desktop/src/gameLayer/SpawnManager.java
@@ -0,0 +1,5 @@
+package gameLayer;
+
+public class SpawnManager {
+
+}
